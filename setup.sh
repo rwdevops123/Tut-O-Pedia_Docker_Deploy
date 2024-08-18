3f7675279327b1ec0be2994baf5bd233b72a9b3f
RuWelDevOps <rudi.welter.devops@gmail.com>
2024-08-18 18:31:46 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ if [ ! "$(docker network ls | grep postgres_network)" ]; then
   echo "Creating postgres network ..."
   docker network create postgres_network
 else
-  echo "Network postgres exists ..."
+  echo "Network postgres exists already ..."
 fi
